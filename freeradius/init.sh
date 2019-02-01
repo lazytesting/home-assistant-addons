a3ef03bd9546ff53b593cab999b03e5ed9445c5e
Maarten Groeneweg <maarten@groeneweg.eu>
2019-02-01 17:58:29 +0100

    moved init script to run

--- freeradius/init.sh
+++ freeradius/init.sh
@@ -10,5 +10,6 @@ DEVICE_HOSTNAME=$(jq --raw-output ".devicehostname" $CONFIG_PATH)
 DEVICE_SECRET=$(jq --raw-output ".devicesecret" $CONFIG_PATH)
 ################
  
+echo "Running init script"
 echo "$USERNAME Cleartext-Password := \"$PASSWORD\"" >> /etc/raddb/users
 echo -e "client $DEVICE_NAME {\n ipaddr = $DEVICE_HOSTNAME\n secret = $DEVICE_SECRET\n}" >> /etc/raddb/clients.conf
