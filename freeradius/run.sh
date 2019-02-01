df111dd1df2c2170de052d629e14b3dee786ddf8
Maarten Groeneweg <maarten@groeneweg.eu>
2019-02-01 18:12:38 +0100

    added run.sh

---
+++ freeradius/run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#### config ####
+CONFIG_PATH=/data/options.json
+
+USERNAME=$(jq --raw-output ".username" $CONFIG_PATH)
+PASSWORD=$(jq --raw-output ".password" $CONFIG_PATH)
+DEVICE_NAME=$(jq --raw-output ".devicename" $CONFIG_PATH)
+DEVICE_HOSTNAME=$(jq --raw-output ".devicehostname" $CONFIG_PATH)
+DEVICE_SECRET=$(jq --raw-output ".devicesecret" $CONFIG_PATH)
+################
+ 
+echo "Setting Freeradius configuration"
+echo "$USERNAME Cleartext-Password := \"$PASSWORD\"" >> /etc/raddb/users
+echo -e "client $DEVICE_NAME {\n ipaddr = $DEVICE_HOSTNAME\n secret = $DEVICE_SECRET\n}" >> /etc/raddb/clients.conf
+
+
+echo "Start Freeradius"
+radiusd -xx -f
